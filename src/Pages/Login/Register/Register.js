ded58fd0be47424abaf259cf870eddbe212dfbf1
Esh shatabdi <eshshatabdi@gmail.com>
2022-04-13 16:10:33 -0400

    google signIn

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import './Register.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 
 const Register = () => {
@@ -41,6 +42,7 @@ const Register = () => {
                 <input type="submit" value="Register" />
             </form>
             <p>Already Have an account? <Link to='/login' className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
+            <SocialLogin></SocialLogin>
 
         </div>
 
